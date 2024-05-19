1e75073d7a7218e12442adb5a1bd9264ecfaaaf8
Tapendra Joshi <tapendra2497@gmail.com>
2024-05-19 14:54:18 +0530

    added constraint for email as unique

--- src/entity/UserSchema.js
+++ src/entity/UserSchema.js
@@ -19,6 +19,7 @@ module.exports = new EntitySchema({
         },
         email: {
             type: "varchar",
+            unique: true
         },
         password: {
             type: "varchar",
